e1ebb7fbbcae0b9aceb872b568822da1e3385ec7
namgeon1106 <namgeon1106@naver.com>
2023-02-15 23:53:27 +0900

    Initial Commit

---
+++ WantedPreOnboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedPreOnboarding
+//
+//  Created by 김남건 on 2023/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
