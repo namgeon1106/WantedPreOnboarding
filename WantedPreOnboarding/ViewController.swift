07cf0f8b09f3016f8cf908479a02e87b74b18e9c
namgeon1106 <namgeon1106@naver.com>
2023-02-18 00:00:12 +0900

    feat: LoadImageView 5개를 VC에 추가

--- WantedPreOnboarding/ViewController.swift
+++ WantedPreOnboarding/ViewController.swift
@@ -8,25 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let vStack = {
+        let vStack = UIStackView()
+        vStack.axis = .vertical
+        vStack.spacing = 10
+        vStack.alignment = .fill
+        
+        return vStack
+    }()
+    
+    let imageLoadViews = (1...5).map { _ in ImageLoadView() }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
         
-        let imageLoadView = ImageLoadView()
-        imageLoadView.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(imageLoadView)
-        
-        NSLayoutConstraint.activate([
-            imageLoadView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            imageLoadView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            imageLoadView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            imageLoadView.heightAnchor.constraint(equalToConstant: 100)
-        ])
+        setUp()
     }
 
-
+    private func setUp() {
+        vStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(vStack)
+        
+        NSLayoutConstraint.activate([
+            vStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            vStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            vStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+        ])
+        
+        imageLoadViews.forEach { [unowned vStack] imageLoadView in
+            imageLoadView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                imageLoadView.heightAnchor.constraint(equalToConstant: 50)
+            ])
+            
+            vStack.addArrangedSubview(imageLoadView)
+        }
+    }
 }
 
